581b7f5c558491c99c39a67d88a4a892ad2f4f0c
akshayzagad <akshayzagade62@gmail.com>
2025-03-03 23:50:39 +0530

    Enhance StarRating component with customizable color, size, and message support; add Test component for demonstration

--- src/StarRating.js
+++ src/StarRating.js
@@ -11,47 +11,72 @@ const starContainerStyle = {
   gap: "0px",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#ffc419",
+  size = 48,
+  className='',
+  message = [],
+  onSetMovieRating,
+}) {
   const [rating, setRating] = useState(0);
-  const [temRating,setTemRating] = useState(0);
-  
+  const [temRating, setTemRating] = useState(0);
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    size: `${size / 1.5}`,
+  };
+
   function handleRating(rating) {
     setRating(rating);
+    onSetMovieRating(rating);
   }
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star onrate={() => handleRating(i + 1)} key={i} Full={temRating ? temRating>=i+1 : rating >= i+1} onHoverIn={()=> setTemRating(i+1)} onHoverOut={()=>setTemRating(i+1)} />
+          <Star  
+            onrate={() => handleRating(i + 1)}
+            key={i}
+            Full={temRating ? temRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTemRating(i + 1)}
+            onHoverOut={() => setTemRating(i + 1)}
+            color={color}
+          size={size}
+          />
         ))}
       </div>
-      <p style={textStyle}>{temRating || ""}</p>
+      <p style={textStyle}>{message.length === maxRating ? message[temRating ?
+        temRating-1 : rating-1] : temRating || rating || ""}</p>
     </div>
   );
 }
 
-function Star({ onrate, Full,onHoverIn,onHoverOut }) {
+function Star({ onrate, Full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
-    width: "20px",
-    hight: "20px",
+    width: `${size}px`,
+    hight: `${size}px`,
     display: "block",
     cursor: "pointer",
   };
 
   return (
-    <span role="button" style={starStyle} onClick={onrate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onrate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {Full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -60,7 +85,7 @@ function Star({ onrate, Full,onHoverIn,onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating'
 
+function Test(){
+  const[movieRating,setMovieRating] = useState(0);
+  return(
+    <div>
+      <StarRating maxRating={5} onSetMovieRating={setMovieRating} color='blue'/>
+  <p>Your movie rate is {movieRating}</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10}/>
+    <StarRating size={25} color='red' className='test' message={['Terrible','Bad','Ok','Good','Amazing']}/>
+    <Test />
   </React.StrictMode>
 );
 
